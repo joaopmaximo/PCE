5816d2f96e0fba3993dd94cea298257aabf5db99
João Pedro Máximo <joao.maximo@fatec.com>
2024-01-19 15:59:40 -0300

    inicializando projeto

---
+++ src/main/java/br/unisantos/pce/PceApplication.java
@@ -0,0 +1,13 @@
+package br.unisantos.pce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/unisantos/pce/PceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unisantos.pce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
