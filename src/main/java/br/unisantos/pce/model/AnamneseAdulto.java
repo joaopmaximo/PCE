06abeea3517f58186632726e660bb2078cb9fce9
João Pedro Máximo <joao.maximo@fatec.com>
2024-01-24 16:22:49 -0300

    att

---
+++ src/main/java/br/unisantos/pce/model/AnamneseAdulto.java
@@ -0,0 +1,29 @@
+package br.unisantos.pce.model;
+
+import java.io.Serial;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class AnamneseAdulto {
+    @Serial
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+
+    @Column(name = "nome", length = 30, nullable = false)
+	private String nome;
+}
