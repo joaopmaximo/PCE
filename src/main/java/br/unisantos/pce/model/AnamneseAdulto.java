162691ee44458b6ac62d59b624a1de39642340ab
joaopmaximo <jp.maximoo@gmail.com>
2024-01-26 04:20:22 -0300

    anamnese

--- src/main/java/br/unisantos/pce/model/AnamneseAdulto.java
+++ src/main/java/br/unisantos/pce/model/AnamneseAdulto.java
@@ -1,7 +1,7 @@
 package br.unisantos.pce.model;
 
 import java.io.Serial;
-
+import java.time.LocalDate;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -9,6 +9,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,12 +20,33 @@ import lombok.NoArgsConstructor;
 @Entity
 public class AnamneseAdulto {
 
-	public enum Tipo_Atendimento {
-		avaliação,
-		orientação
+	private enum TipoAtendimento {
+		avaliacao,
+		orientacao
+	}
+
+	private enum Sexo {
+		masculino,
+		feminino,
+		nao_informado
 	}
 	
-    @Serial
+	private enum Escolaridade {
+		fundamental_completo,
+		ensino_medio_completo,
+		ensino_superior_incompleto,
+		ensino_superior_completo,
+		outra
+	}
+	
+	private enum Periodo {
+		manha,
+		tarde,
+		noite,
+		NA
+	}
+
+	@Serial
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -32,11 +54,44 @@ public class AnamneseAdulto {
 	@Column(name = "id")
 	private Integer id;
 
-    @Column(name = "nome", length = 30, nullable = false)
+	@Column(name = "nome", length = 30, nullable = false)
 	private String nome;
 
 	@Column(name = "tipo_atendimento", nullable = false)
 	@Enumerated(EnumType.STRING)
-	private Tipo_Atendimento tipo_Atendimento;
+	private TipoAtendimento tipo_Atendimento;
+
+	@Column(name = "data_nascimento", nullable = false)
+	private LocalDate data_nascimento;
+
+	@Column(name = "idade", nullable = false)
+	private Integer idade;
+
+	@Column(name = "sexo", nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Sexo sexo;
+
+	@Column(name = "cidade", nullable = false);
+	private String cidade;
+
+	@Column(name = "bairro", nullable = false);
+	private String bairro;
+
+	@Column(name = "data_atendimento", nullable = false, updatable = false)
+	private LocalDate data_atendimento;
+
+	@PrePersist
+	protected void onCreate() {
+		data_atendimento = LocalDate.now();
+		Integer idadeAux = LocalDate.now().getYear() - data_nascimento.getYear();
+
+		if (data_nascimento.getMonthValue() > LocalDate.now().getMonthValue() ||
+				(data_nascimento.getMonthValue() == LocalDate.now().getMonthValue() &&
+						data_nascimento.getDayOfMonth() > LocalDate.now().getDayOfMonth())) {
+			idade = idadeAux--;
+		} else {
+			idade = idadeAux;
+		}
+	}
 
 }
